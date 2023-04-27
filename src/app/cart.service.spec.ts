46cbabf0151f6d92a4f1898a4fa42977d7a38c6f
Christopher Flinchbaugh <chris.f@Very-MacBook-Pro.local>
2023-04-26 23:30:13 -0400

    Fix broken tests

--- src/app/cart.service.spec.ts
+++ src/app/cart.service.spec.ts
@@ -1,16 +1,57 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { CartService } from './cart.service';
+import { Product } from './products';
 
 describe('CartService', () => {
   let service: CartService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ CartService ]
+    });
+
     service = TestBed.inject(CartService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add a product to the cart', () => {
+    const product: Product = { id: 1, name: 'Test Product', price: 10, description: '' };
+    service.addToCart(product);
+    expect(service.getItems().length).toBe(1);
+    expect(service.getItems()[0]).toEqual(product);
+  });
+
+  it('should clear the cart', () => {
+    const product: Product = { id: 1, name: 'Test Product', price: 10, description: '' };
+    service.addToCart(product);
+    service.clearCart();
+    expect(service.getItems().length).toBe(0);
+  });
+
+  it('should get shipping prices', () => {
+    const mockShippingPrices = [
+      { type: 'Standard', price: 5 },
+      { type: 'Express', price: 10 }
+    ];
+
+    service.getShippingPrices().subscribe((shippingPrices) => {
+      expect(shippingPrices).toEqual(mockShippingPrices);
+    });
+
+    const req = httpMock.expectOne('/assets/shipping.json');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockShippingPrices);
   });
 });
--- src/app/shipping/shipping.component.spec.ts
+++ src/app/shipping/shipping.component.spec.ts
@@ -1,23 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShippingComponent } from './shipping.component';
+import { CartService } from '../cart.service';
+import { of } from 'rxjs';
 
 describe('ShippingComponent', () => {
   let component: ShippingComponent;
   let fixture: ComponentFixture<ShippingComponent>;
+  let cartService: jasmine.SpyObj<CartService>;
 
   beforeEach(async () => {
+    const cartServiceSpy = jasmine.createSpyObj('CartService', ['getShippingPrices']);
+
     await TestBed.configureTestingModule({
-      declarations: [ ShippingComponent ]
+      declarations: [ ShippingComponent ],
+      providers: [{ provide: CartService, useValue: cartServiceSpy }]
     })
     .compileComponents();
 
+    cartService = TestBed.inject(CartService) as jasmine.SpyObj<CartService>;
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(ShippingComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get shipping prices', () => {
+    const shippingPrices = [{ type: 'Standard', price: 10 }, { type: 'Express', price: 20 }];
+  
+    cartService.getShippingPrices.and.returnValue(of(shippingPrices));
+  
+    fixture.detectChanges();
+  
+    component.shippingCosts.subscribe(prices => {
+      expect(prices).toEqual(shippingPrices);
+    });
+  });
+
 });
+
